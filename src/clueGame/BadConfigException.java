a3c25442273320b73fd1c25a691e8fb98c41a49f
Peter Taenzer <Ptaenzer@mines.edu>
2017-10-30 20:55:49 -0600

    Added comments to custom exception 'BadConfigException.java'

--- src/clueGame/BadConfigException.java
+++ src/clueGame/BadConfigException.java
@@ -1,14 +1,23 @@
+/*
+ * Authors: Peter Taenzer and Jacob Gay
+ * This is our custom exception class thrown when the configuration isn't correct
+ */
+
 package clueGame;
 
 public class BadConfigException extends Exception {
 	
+	//Generic constructor
 	public BadConfigException() {
 		super("Error: BadConfigException");
 	}
+	
+	//Constructor with parameter
 	public BadConfigException(String s) {
 		super(s + "has a BadConfigException");
 	}
 
+	//Override for printing of this error
 	@Override
 	public String toString() {
 		return "BadConfigException";
