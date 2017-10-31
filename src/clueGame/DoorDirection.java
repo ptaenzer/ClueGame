a1698f1850e2ca062a0f9763ec91196191d5c016
Peter Taenzer <Ptaenzer@mines.edu>
2017-10-30 20:59:17 -0600

    Added comments to enumerated type 'DoorDirection.java'

--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -1,13 +1,23 @@
+/*
+ * Authors: Peter Taenzer and Jacob Gay
+ * This is our enumerated type class for the door directions
+ */
+
 package clueGame;
 
 public enum DoorDirection {
+	//Values for the type
 	UP ("U"), DOWN ("D"), LEFT ("L"), RIGHT ("R"), CORNER ("C"), NONE ("");
+	
+	//declare the instance variable
 	private String value;
 	
+	//Constructor for DoorDirection
 	DoorDirection (String aValue){
 		value = aValue;
 	}
 	
+	//Allows printing of this type
 	public String toString(){
 		return value;
 	}
