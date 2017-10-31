3e6acce9b13303cba6854d0bf3e165a653407e02
Peter Taenzer <Ptaenzer@mines.edu>
2017-10-30 20:35:55 -0600

    Added comments in BoardCell.java

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 public class BoardCell {
 	
+	//declare all instance variables needed for each cell
 	private int row;
 	private int column;
 	private char initial;
@@ -16,6 +17,7 @@ public class BoardCell {
 	boolean isDoor;
 	private Set<BoardCell> adj = new HashSet<BoardCell>();
 
+	//constructor for BoardCell
 	public BoardCell(int row, int column, char initial) {
 		super();
 		this.row = row;
@@ -32,54 +34,55 @@ public class BoardCell {
 		return adj;
 	}
 
+	//returns the row that the cell is in
 	public int getRow() {
 		// TODO Auto-generated method stub
 		return row;
 	}
 
+	//returns the column that the cell is in
 	public int getColumn() {
 		// TODO Auto-generated method stub
 		return column;
 	}
 	
+	//sets the row for this cell
 	public void setRow(int row) {
 		this.row = row;
 	}
 
+	//sets the column for this cell
 	public void setColumn(int column) {
 		this.column = column;
 	}
 	
+	//sets the initial for this cell
 	public void setInitial(char i) {
 		this.initial = i;
 	}
 	
+	//sets the door direction for this cell
 	public void setDoorDirection(DoorDirection d) {
 		this.direction = d;
 	}
 	
+	//sets boolean isDoor to true if it is a door
 	public void setIsDoor() {
 		this.isDoor = true;
 	}
 	
-	public boolean isWalkway() {
-		return false;
-	}
-	
-	public boolean isRoom() {
-		return false;
-	}
-	
+	//returns isDoor for cell that called the method
 	public boolean isDoorway() {
 		return isDoor;
 	}
 
+	//returns the initial for the cell that called the method
 	public char getInitial() {
-		return this.initial;
+		return initial;
 	}
 
+	//returns the door direction for the cell that called the method
 	public DoorDirection getDoorDirection() {
 		return direction;
 	}
-
 }
